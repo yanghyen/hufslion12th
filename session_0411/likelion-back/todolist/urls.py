40a81ce8b882659de23235c3d09c11823201bd8e
yanghyen <sk0310803@naver.com>
2024-04-17 23:16:56 +0900

    ADD: 240417 TIL 업로드

--- session_0411/likelion-back/todolist/urls.py
+++ session_0411/likelion-back/todolist/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.welcome, name='welcome'),  # welcome.html 페이지 매핑
-    path('work/', views.work, name='work'),   # work.html 페이지 매핑
-    path('work/<int:todo_id>/', views.work, name='work_detail'),   # work.html 조회
-    path('update/<int:todo_id>/', views.update, name='update'), #update url 추가
-    path('delete/<int:todo_id>/', views.delete, name='delete'), # delete url 추가
+    path("", views.welcome, name="welcome"),  # welcome.html 페이지 매핑
+    path("work/", views.work, name="work"),  # work.html 페이지 매핑
+    path("work/<int:todo_id>/", views.work, name="work_detail"),  # work.html 조회
+    path("update/<int:todo_id>/", views.update, name="update"),  # update url 추가
+    path("delete/<int:todo_id>/", views.delete, name="delete"),  # delete url 추가
 ]
