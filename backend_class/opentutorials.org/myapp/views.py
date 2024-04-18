b6041e9454e927b846a3978438ab3316e2dadd0f
yanghyen <sk0310803@naver.com>
2024-04-18 22:17:37 +0900

    ADD: 240418 TIL 업로드

---
+++ backend_class/opentutorials.org/myapp/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, HttpResponse
+
+topics = [
+  {'id':1, 'title':'routing', 'body':'Routing is ..'},
+  {'id':2, 'title':'view', 'body':'View is ..'},
+  {'id':3, 'title':'model', 'body':'Model is ..'},
+]
+
+def HTMLTemplate(articleTag):
+  global topics
+  ol = ''
+  for topic in topics:
+    ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
+  return f'''
+  <html>
+  <body>
+      <h1><a href="/">Django</a></h1>
+      <ul>
+          {ol}
+      </ul>
+      {articleTag}
+  </body>
+  </html>
+  '''
+
+def index(request):
+  article = '''
+  <h2>Welcome</h2>
+  Hello, Django
+  '''
+  return HttpResponse(HTMLTemplate(article))
+
+def read(request, id):
+  global topics
+  article = ''
+  for topic in topics:
+    if topic['id'] == int(id):
+      article = f'<h2>{topic["title"]}</h2>{topic["body"]}'
+  return HttpResponse(HttpResponse(article))
+
+def create(request):
+  return HttpResponse('Create')
+
+
--- backend_class/opentutorials.org/myproject/urls.py
+++ backend_class/opentutorials.org/myproject/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+# http://127.0.0.1/
+# http://127.0.0.1/app/
+
+# http://127.0.0.1/create/
+# http://127.0.0.1/read/1/
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', include('myapp.urls'))
 ]
