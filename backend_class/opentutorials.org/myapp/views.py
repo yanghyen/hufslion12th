b8158c6ab88f8864402f153e3714c81dcf90e599
yanghyen <sk0310803@naver.com>
2024-05-03 17:53:26 +0900

    ADD: 240503 파일 업로드

--- backend_class/opentutorials.org/myapp/views.py
+++ backend_class/opentutorials.org/myapp/views.py
@@ -79,12 +79,30 @@ def create(request):
     
 @csrf_exempt
 def update(request, id):
+   global topics
    if request.method == 'GET':
-      article = 'Update'
+      for topic in topics:
+         if topic['id'] == int(id):
+            selectedTopic = {
+               "title":topic['title'],
+               "body":topic['body']
+            }
+      article =  f'''
+            <form action="/update/{id}/" method="post">
+                <p><input type="text" name="title" placeholder="title" value={selectedTopic['title']}></p>
+                <p><textarea name="body" placeholder="body">{selectedTopic['body']}</textarea></p>
+                <p><input type="submit"></p> 
+            </form>
+        '''
       return HttpResponse(HTMLTemplate(article, id))
    elif request.method == 'POST':
+      title = request.POST['title']
+      body = request.POST['body']
+      for topic in topics:
+         if topic['id'] == int(id):
+            topic['title'] = title
+            topic['body'] = body
       return redirect(f'/read/{id}')
-
 
 
 @csrf_exempt
