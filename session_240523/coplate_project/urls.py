10f3f8e154e39b4d812b3a9b16853bacb1690cb4
yanghyen <sk0310803@naver.com>
2024-06-02 10:00:11 +0900

    ADD: 240523 session upload

---
+++ session_240523/coplate_project/urls.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import TemplateView
+from coplate.views import CustomPasswordChangeView
+
+urlpatterns = [
+    # admin
+    path('admin/', admin.site.urls),
+    # coplate
+    path('', include('coplate.urls')),
+    # allauth
+    path("email-confirmation-done/",
+         TemplateView.as_view(template_name="coplate/email_confirmation_done.html"),
+         name= "account_email_confirmation_done",
+         ),
+    path('password/change/',
+		     CustomPasswordChangeView.as_view(),
+		     name="account_charge_password",
+		     ),
+    path('', include('allauth.urls')),
+]
+
