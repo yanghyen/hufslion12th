533edfa28db69c39212a0f873f59bd5993a3bb5e
yanghyen <sk0310803@naver.com>
2024-06-18 15:39:00 +0900

    ADD: 5주차 실습 코드 추가

---
+++ backend_class/TECHITJava/likelion_java/src/Ch5_Variables.java
@@ -0,0 +1,19 @@
+public class Ch5_Variables {
+    public static void main(String[] args) {
+        // 변수 -> 데이터가 저장되는 공간
+
+        // 1. 1개의 변수 => 1개의 값만 할당 가능
+        // 2. 변수에 저장된 값 => 재할당을 통해 변경 가능
+        // 3. 값의 형태에 맞는 자료형을 사용
+        // 4. 변수명은 소문자로 시작
+        // 5. 대소문자 구분, 공백 포함 불가능
+        // 6. 자바 예약어 사용 불가  ex) int, double, ...
+//        int num = 1;
+//        double width = 12.34;
+//        String content = "Programming";
+//        num = 10; // 재할당할 때는 int 안 써도 됨
+
+
+
+    }
+}
---
+++ backend_class/TECHITJava/likelion_java/src/JavaReview.java
@@ -0,0 +1,7 @@
+public class JavaReview {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        // sout System.out.println(); 약어 얏호
+
+    }
+}
